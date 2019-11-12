85434409292ca1a118b43450fe26815083be5966
Hector DelaRosa <hdelaros@gmail.com>
2019-11-12 16:42:27 -0500

    Some minor updates to db uri

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize'); //uppercase due to being a class constructor
-const db = new Sequelize('postgres://localhost/nouns_db');
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/nouns_db', {logging: false});
 // const Op = Sequelize.Op;
 const chalk = require('chalk');
 
@@ -34,54 +34,19 @@ Places.hasMany(Persons);
 Persons.belongsTo(Things);
 Things.hasMany(Persons);
 
-// db.sync({ force: true});
-
-// const Product = db.define('product', {
-//     id: {
-//         primaryKey: true,
-//         type: Sequelize.UUID,
-//         defaultValue: Sequelize.UUIDV4
-//     },
-//     name: {
-//         type: Sequelize.STRING,
-//         allowNull: false,
-//         unique: true
-//     },
-//     inStock: {
-//         type: Sequelize.BOOLEAN,
-//         allowNull: false,
-//         defaultValue: true
-//     }
-// }); //model use Pascal case
-// const Category = db.define('category',{
-//     id: {
-//         primaryKey: true,
-//         type: Sequelize.UUID,
-//         defaultValue: Sequelize.UUIDV4
-//     },
-//     name: {
-//         type: Sequelize.STRING,
-//         allowNull: false,
-//         unique: true
-//     }
-// }); //model use Pascal case
-
-// Product.belongsTo(Category);
-// Category.hasMany(Product);
-
 const syncAndSeed = async () => {
   try {
     await db.sync({
       force: true,
     });
     const [fullstack, trainStation] = await Promise.all([
-        Places.create({ name: 'Fullstack' }),
-        Places.create({ name: 'trainstation' }),
-      ]);
-      const [laptop, phone] = await Promise.all([
-        Things.create({ name: 'laptop' }),
-        Things.create({ name: 'phone' }),
-      ]);
+      Places.create({ name: 'Fullstack' }),
+      Places.create({ name: 'trainstation' }),
+    ]);
+    const [laptop, phone] = await Promise.all([
+      Things.create({ name: 'laptop' }),
+      Things.create({ name: 'phone' }),
+    ]);
     const [moe, larry, curly] = await Promise.all([
       Persons.create({
         name: 'Moe',
@@ -99,38 +64,15 @@ const syncAndSeed = async () => {
         thingId: laptop.id,
       }),
     ]);
-    
-    
   } catch (err) {
     console.log(chalk.red(err, err.message));
   }
 };
 module.exports = {
-    syncAndSeed,
-    models:{
-        Persons, Places, Things,
-    }
-}
-
-//     const products = await Product.findAll({
-//         where:
-//         {
-//             name: {
-//                 [Op.like]: '%foo%',
-//             }
-//         }
-//     });
-//     products.forEach( product => {
-//         console.log(product.name);
-//     })
-//     } catch(err){
-//         console.log(chalk.red(err));
-//     }
-// };
-
-// syncAndSeed();
-
-// const pg = require('pg');
-// const { Client } = pg;
-// const client = new Client('postgres://localhost:5432/acme_nouns_db');
-// client.connect();
+  syncAndSeed,
+  models: {
+    Persons,
+    Places,
+    Things,
+  },
+};
